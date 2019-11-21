f9b8c2f55630635958778cb1e9d4e5c69f3d0237
Ghozizzz <ghozi98@yahoo.com>
2019-11-21 11:07:06 +0700

    yang kurang

---
+++ config/global.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'hashLogin' => 'FgaThjSkiwLkicoJklpasUyhTg',
+    'noreply_email' => 'noreply@zavile.com',
+    'jne_key' => 'c2c73534e9fe8089f9f73547d43d5f1e', 
+    'mataharimall_key' => '387acf55aeb0bf7308f74c5617932363',
+    'sandbox' => [
+        'host' => 'http://apiseller.mataharimall.net',
+    ],
+    'prod' => [
+        'host' => 'https://apiseller.mataharimall.com',
+    ],
+];
+        
---
+++ config/services.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for third party services such
+    | as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+    | default location for this type of information, allowing packages
+    | to have a conventional place to find your various credentials.
+    |
+    */
+
+    'mailgun' => [
+        'domain' => 'sandbox532a5675486c4ed69e3fe1e199589ad3.mailgun.org',
+        'secret' => '8720da34f58ad0c07479bf280d9f2509-770f03c4-d1884915',
+    ],
+
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => 'us-east-1',
+    ],
+
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+
+];
